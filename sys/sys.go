9c2bf60320dacc490e4b0f5338eda43f1bfa7d9f
donnie4w <donnie4w@gmail.com>
2024-03-07 11:22:32 +0800

    Create sys.go

---
+++ sys/sys.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/wfs
+package sys
+
+import (
+	"time"
+)
+
+type server struct{}
+
+func (s *server) Serve() error {
+	praseflag()
+	blankLine()
+	wfslogo()
+	KeyStoreInit(WFSDATA)
+	writePid(WFSDATA)
+	Serve.BackForEach(func(_ int, s Server) bool {
+		go s.Serve()
+		<-time.After(time.Millisecond << 9)
+		return true
+	})
+	select {}
+}
+
+func (s *server) Close() (err error) {
+	Serve.FrontForEach(func(_ int, s Server) bool {
+		defer func() { recover() }()
+		s.Close()
+		return true
+	})
+	return
+}
