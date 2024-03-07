d040111fae5cf355b5ef1827f34080ca5de426ac
donnie4w <donnie4w@gmail.com>
2024-03-07 11:20:42 +0800

    level1

---
+++ level1/process.go
@@ -0,0 +1,138 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/wfs
+
+package level1
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+
+	goutil "github.com/donnie4w/gofer/util"
+	"github.com/donnie4w/gothrift/thrift"
+	. "github.com/donnie4w/wfs/stub"
+	"github.com/donnie4w/wfs/sys"
+	"github.com/donnie4w/wfs/util"
+)
+
+func init() {
+	sys.Serve.Put(3, server)
+}
+
+var _transportFactory = thrift.NewTBufferedTransportFactory(1 << 13)
+var _tcompactProtocolFactory = thrift.NewTCompactProtocolFactoryConf(&thrift.TConfiguration{})
+
+var server = &service{}
+
+type service struct {
+	isClose         bool
+	serverTransport thrift.TServerTransport
+}
+
+func (t *service) _server(_addr string, processor thrift.TProcessor, TLS bool, serverCrt, serverKey string) (err error) {
+	if TLS {
+		cfg := &tls.Config{}
+		var cert tls.Certificate
+		if cert, err = tls.LoadX509KeyPair(serverCrt, serverKey); err == nil {
+			cfg.Certificates = append(cfg.Certificates, cert)
+			t.serverTransport, err = thrift.NewTSSLServerSocketTimeout(_addr, cfg, sys.SocketTimeout)
+		}
+	} else {
+		t.serverTransport, err = thrift.NewTServerSocketTimeout(_addr, sys.SocketTimeout)
+	}
+
+	if err == nil && t.serverTransport != nil {
+		server := thrift.NewTSimpleServer4(processor, t.serverTransport, nil, nil)
+		if err = server.Listen(); err == nil {
+			s := fmt.Sprint("operating services start[", _addr, "]")
+			if TLS {
+				s = fmt.Sprint("operating services start tls[", _addr, "]")
+			}
+			sys.FmtLog(s)
+			for {
+				if _transport, err := server.ServerTransport().Accept(); err == nil {
+					go func() {
+						defer util.Recover()
+						cc := newCliContext(_transport)
+						defer cc.close()
+						defaultCtx := context.WithValue(context.Background(), "CliContext", cc)
+						if inputTransport, err := _transportFactory.GetTransport(_transport); err == nil {
+							inputProtocol := _tcompactProtocolFactory.GetProtocol(inputTransport)
+							for {
+								ok, err := processor.Process(defaultCtx, inputProtocol, inputProtocol)
+								if errors.Is(err, thrift.ErrAbandonRequest) {
+									break
+								}
+								if errors.As(err, new(thrift.TTransportException)) && err != nil {
+									break
+								}
+								if !ok {
+									break
+								}
+							}
+						}
+					}()
+				}
+			}
+		}
+	}
+	if !t.isClose && err != nil {
+		fmt.Println("operating services start failed:", err)
+		sys.Wfs.Close()
+		os.Exit(1)
+	}
+	return
+}
+
+func (t *service) Close() (err error) {
+	defer util.Recover()
+	if strings.TrimSpace(sys.OPADDR) != "" {
+		t.isClose = true
+		err = t.serverTransport.Close()
+	}
+	return
+}
+
+func (t *service) Serve() (err error) {
+	if strings.TrimSpace(sys.OPADDR) != "" {
+		tls := false
+		if sys.Conf.Admin_Ssl_crt != "" && sys.Conf.Admin_Ssl_crt_key != "" {
+			tls = true
+		}
+		err = t._server(strings.TrimSpace(sys.OPADDR), NewWfsIfaceProcessor(processor), tls, sys.Conf.Admin_Ssl_crt, sys.Conf.Admin_Ssl_crt_key)
+	} else {
+		sys.FmtLog("no operating services")
+	}
+	return
+}
+
+type pcontext struct {
+	Id       int64
+	isAuth   bool
+	tt       thrift.TTransport
+	mux      *sync.Mutex
+	_isClose bool
+}
+
+func newCliContext(tt thrift.TTransport) (cc *pcontext) {
+	cc = &pcontext{goutil.RandId(), false, tt, &sync.Mutex{}, false}
+	return
+}
+
+func (t *pcontext) close() {
+	defer util.Recover()
+	defer t.mux.Unlock()
+	t.mux.Lock()
+	if !t._isClose {
+		t._isClose = true
+		t.tt.Close()
+	}
+}
---
+++ level1/processor.go
@@ -0,0 +1,135 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/wfs
+package level1
+
+import (
+	"context"
+	"strings"
+
+	goutil "github.com/donnie4w/gofer/util"
+	. "github.com/donnie4w/wfs/keystore"
+	. "github.com/donnie4w/wfs/stub"
+	"github.com/donnie4w/wfs/sys"
+	"github.com/donnie4w/wfs/util"
+)
+
+type processhandle struct {
+}
+
+var processor = &processhandle{}
+
+func ctx2CliContext(ctx context.Context) *pcontext {
+	return ctx.Value("CliContext").(*pcontext)
+}
+
+// Parameters:
+//   - I
+func (t *processhandle) Ping(ctx context.Context) (_r int8, _err error) {
+	defer util.Recover()
+	mux := ctx2CliContext(ctx).mux
+	defer mux.Unlock()
+	mux.Lock()
+	_r = 1
+	return
+}
+
+// Parameters:
+//   - File
+func (t *processhandle) Append(ctx context.Context, wf *WfsFile) (_r *WfsAck, _err error) {
+	defer util.Recover()
+	cc := ctx2CliContext(ctx)
+	defer cc.mux.Unlock()
+	cc.mux.Lock()
+	if noAuthAndClose(cc) {
+		_err = sys.ERR_AUTH.Error()
+		return
+	}
+	_r = &WfsAck{Ok: true}
+	if wf.Name != "" && wf.Data != nil && len(wf.Data) > 0 {
+		if int64(len(wf.Data)) > sys.DataMaxsize {
+			_r.Ok, _r.Error = false, sys.ERR_OVERSIZE.WfsError()
+			return
+		}
+		compress := sys.CompressType
+		if wf.Compress != nil {
+			compress = int32(*wf.Compress)
+		}
+		if err := sys.AppendData(wf.Name, wf.Data, compress); err != nil {
+			_r.Ok, _r.Error = false, err.WfsError()
+		}
+	}
+	return
+}
+
+// Parameters:
+//   - Path
+func (t *processhandle) Delete(ctx context.Context, path string) (_r *WfsAck, _err error) {
+	defer util.Recover()
+	cc := ctx2CliContext(ctx)
+	defer cc.mux.Unlock()
+	cc.mux.Lock()
+	if noAuthAndClose(cc) {
+		_err = sys.ERR_AUTH.Error()
+		return
+	}
+	_r = &WfsAck{Ok: true}
+	if path != "" {
+		if err := sys.DelData(path); err != nil {
+			_r.Ok, _r.Error = false, err.WfsError()
+		}
+	}
+	return
+}
+
+// Parameters:
+//   - Path
+func (t *processhandle) Get(ctx context.Context, path string) (_r *WfsData, _err error) {
+	defer util.Recover()
+	cc := ctx2CliContext(ctx)
+	defer cc.mux.Unlock()
+	cc.mux.Lock()
+	if noAuthAndClose(cc) {
+		_err = sys.ERR_AUTH.Error()
+		return
+	}
+	_r = &WfsData{}
+	if path != "" {
+		_r.Data = sys.GetData(path)
+	}
+	return
+}
+
+// Parameters:
+//   - Wa
+func (t *processhandle) Auth(ctx context.Context, wa *WfsAuth) (_r *WfsAck, _err error) {
+	defer util.Recover()
+	mux := ctx2CliContext(ctx).mux
+	mux.Lock()
+	defer mux.Unlock()
+	_r = &WfsAck{Ok: true}
+	if wa.Name != nil && wa.Pwd != nil && auth(*wa.Name, *wa.Pwd) {
+		ctx2CliContext(ctx).isAuth = true
+	} else {
+		_r.Ok, _r.Error = false, sys.ERR_NOPASS.WfsError()
+	}
+	return
+}
+
+func auth(name, pwd string) (b bool) {
+	if _r, ok := Admin.GetAdmin(name); ok {
+		b = strings.EqualFold(_r.Pwd, goutil.Md5Str(pwd))
+	}
+	return
+}
+
+func noAuthAndClose(cc *pcontext) (b bool) {
+	if !cc.isAuth {
+		cc.tt.Close()
+		b = true
+	}
+	return
+}
