2d415ed8aa2949ccca0a44e371e244e576b70169
donnie4w <donnie4w@gmail.com>
2024-03-12 01:30:01 +0800

    Update err.go

--- sys/err.go
+++ sys/err.go
@@ -13,16 +13,19 @@ import (
 )
 
 var ERR_OVERSIZE = err(4101, "data oversize")
-var ERR_NOPASS = err(4102, "no pass")
-var ERR_PARAMS = err(4103, "params error")
+var ERR_NOPASS = err(4102, "verification fail")
+var ERR_PARAMS = err(4103, "parameter error")
 var ERR_EXSIT = err(4104, "has exist")
 var ERR_AUTH = err(4105, "limited authority")
 var ERR_NOTEXSIT = err(4106, "not exist")
+var ERR_NEWPATHEXIST = err(4107, "the new path already exists")
 
 var ERR_UNDEFINED = err(5101, "undefined error")
 var ERR_DEFRAG_FORBID = err(5102, "operation files cannot be defragmented")
 var ERR_DEFRAG_UNDERWAY = err(5103, "defragmentation is underway")
 var ERR_STOPSERVICE = err(5104, "service has stopped")
+var ERR_FILEAPPEND = err(5105, "append data error")
+var ERR_FILECREATE = err(5106, "create file error")
 
 type ERROR interface {
 	WfsError() *WfsError
