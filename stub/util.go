cf27985872b1f1ccfcb45a76820fa4b04cbb6cea
donnie4w <donnie4w@gmail.com>
2024-03-13 23:27:45 +0800

    Update util.go

--- stub/util.go
+++ stub/util.go
@@ -1,3 +1,10 @@
+// Copyright (c) 2023, donnie <donnie4w@gmail.com>
+// All rights reserved.
+// Use of t source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+// github.com/donnie4w/wfs
+
 package stub
 
 import "google.golang.org/protobuf/proto"
