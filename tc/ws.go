dd8c2bff287d24eac91fb4e8adc96ddbd8db1122
donnie4w <donnie4w@gmail.com>
2024-03-13 23:27:57 +0800

    Update ws.go

--- tc/ws.go
+++ tc/ws.go
@@ -8,25 +8,23 @@
 package tc
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/donnie4w/wfs/keystore"
 	"github.com/donnie4w/wfs/sys"
 )
 
-func init() {
-
-}
-
-func newAdminLocalWsClient(tls bool, opaddr, requri, name, pwd string) (ws *sys.WS, err error) {
-	procol := "ws://"
-	if tls {
-		procol = "wss://"
-	}
-	if err = keystore.LoadAdmin(sys.WFSDATA + "/logs"); err != nil {
-		return
-	}
+func newAdminLocalWsClient(tls bool, pid, opaddr, requri, name, pwd string) (ws *sys.WS, err error) {
+	local := false
 	if opaddr == "" {
+		local = true
+		if pid == "" {
+			pid = sys.WFSDATA
+		}
+		if err = keystore.LoadAdmin(pid + "/logs"); err != nil {
+			return nil, fmt.Errorf("wfs data directory cannot be parsed:%s", pid)
+		}
 		if v, ok := keystore.Admin.GetOther("webaddr"); ok {
 			if strings.HasPrefix(v, ":") {
 				opaddr = "127.0.0.1" + v
@@ -34,8 +32,12 @@ func newAdminLocalWsClient(tls bool, opaddr, requri, name, pwd string) (ws *sys.
 				opaddr = v
 			}
 		}
-	}
-	if name == "" {
+		if sys.Conf.Admin_Ssl_crt != "" && sys.Conf.Admin_Ssl_crt_key != "" {
+			tls = true
+		} else {
+			tls = false
+		}
+
 		ss := keystore.Admin.AdminList()
 		if len(ss) > 0 {
 			name = ss[0]
@@ -44,6 +46,15 @@ func newAdminLocalWsClient(tls bool, opaddr, requri, name, pwd string) (ws *sys.
 			}
 		}
 	}
-	ws, err = sys.NewWfsClient(procol+opaddr+requri, "http://127.0.0.1", name, pwd)
+	proto := "ws://"
+	if tls {
+		proto = "wss://"
+	}
+
+	if ws, err = sys.NewWfsClient(proto+opaddr+requri, "http://127.0.0.1", name, pwd); err != nil {
+		if !local {
+			err = fmt.Errorf("wfs service cannot be connected, host[%s],tls[%t],username[%s],password[%s]", opaddr, tls, name, pwd)
+		}
+	}
 	return
 }
