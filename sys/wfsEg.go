cc4eb730729e66d9c1cdcbb46cd62a173300600f
donnie4w <donnie4w@gmail.com>
2024-05-10 01:04:20 +0800

    Update wfsEg.go

--- sys/wfsEg.go
+++ sys/wfsEg.go
@@ -13,24 +13,25 @@ import (
 )
 
 var (
-	KeyStoreInit  func(string)
-	Count         func() int64
-	Seq           func() int64
-	AppendData    func(string, []byte, int32) (int64, ERROR)
-	GetData       func(string) []byte
-	DelData       func(string) ERROR
-	Add           func([]byte, []byte) error
-	Del           func([]byte) error
-	SearchLike    func(string) []*PathBean
-	SearchLimit   func(int64, int64) []*PathBean
-	Defrag        func(string) ERROR
-	FragAnalysis  func(string) (*FragBean, ERROR)
-	Export        func(func(*stub.SnapshotBean) bool) error
-	ExportByINCR  func(int64, int64, func(*stub.SnapshotBeans) bool) ERROR
-	ExportFile    func(int64, int64, func(*stub.SnapshotFile) bool) ERROR
-	ExportByPaths func([]string, func(snaps *stub.SnapshotBeans) bool) ERROR
-	Import        func(*stub.SnapshotBean, bool) error
-	ImportFile    func(*stub.SnapshotFile) ERROR
-	Modify        func(string, string) ERROR
-	WsClient      func(tls bool, pid, opaddr, requri, name, pwd string) (ws *WS, err error)
+	KeyStoreInit   func(string)
+	Count          func() int64
+	Seq            func() int64
+	AppendData     func(string, []byte, int32) (int64, ERROR)
+	GetData        func(string) []byte
+	DelData        func(string) ERROR
+	Add            func([]byte, []byte) error
+	Del            func([]byte) error
+	SearchLike     func(string) []*PathBean
+	SearchLimit    func(int64, int64) []*PathBean
+	Defrag         func(string) ERROR
+	FragAnalysis   func(string) (*FragBean, ERROR)
+	Export         func(func(*stub.SnapshotBean) bool) error
+	ExportByINCR   func(int64, int64, func(*stub.SnapshotBeans) bool) ERROR
+	ExportFile     func(int64, int64, func(*stub.SnapshotFile) bool) ERROR
+	ExportByPaths  func([]string, func(snaps *stub.SnapshotBeans) bool) ERROR
+	Import         func(*stub.SnapshotBean, bool) error
+	ImportFile     func(*stub.SnapshotFile) ERROR
+	Modify         func(string, string) ERROR
+	WsClient       func(tls bool, pid, opaddr, requri, name, pwd string) (ws *WS, err error)
+	IsEmptyBigFile func(string) bool
 )
